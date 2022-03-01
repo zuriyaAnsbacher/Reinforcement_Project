b8cf716ae6742dbc7914258b90bd551efafac0d7
zuriyaAnsbacher <zuriya.an@gmail.com>
2022-03-01 15:46:53 +0200

    add dueling model

--- DQN/Models.py
+++ DQN/Models.py
@@ -14,3 +14,26 @@ class DQN(nn.Module):
         x = self.fc2(x)
         x = self.fc3(x)
         return x
+
+
+class DuelingDQN(nn.Module):
+    def __init__(self, input_size, output_size, linear_hid_size, adv_hid_size, val_hid_size):
+        super(DuelingDQN, self).__init__()
+
+        # common linear layer
+        self.linear1 = nn.Linear(input_size, linear_hid_size)
+
+        # 2 linear layers for advantage calculation
+        self.linear_adv_1 = nn.Linear(linear_hid_size, adv_hid_size)
+        self.linear_adv_2 = nn.Linear(adv_hid_size, output_size)
+
+        # 2 linear layers for value calculation
+        self.linear_val_1 = nn.Linear(linear_hid_size, val_hid_size)
+        self.linear_val_2 = nn.Linear(val_hid_size, 1)
+
+    def forward(self, state):
+        x = nn.ReLU(self.linear1(state))
+        adv = self.linear_adv_2(nn.ReLU(self.linear_adv_1(x)))
+        val = self.linear_val_2(nn.ReLU(self.linear_val_1(x)))
+
+        return val + (adv - adv.mean())
